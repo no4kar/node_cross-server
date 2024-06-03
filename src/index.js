f6a4824b4a6688ea360389784f88e78e92197969
Bahtijar <no4kar@gmail.com>
2024-06-03 22:30:09 +0200

    fix: send headers,body

--- src/index.js
+++ src/index.js
@@ -10,12 +10,23 @@ const serverUrl = process.env.URL || 'http://ec2-13-53-106-198.eu-north-1.comput
 // Enable CORS
 app.use(cors());
 
+// Use express.json() and express.urlencoded() to handle request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.all('*', (req, res) => {
   const url = serverUrl + req.url;
+  console.info(`Forwarding ${req.method} request to: ${url}`);
 
-  console.info(url);
+  const options = {
+    url: url,
+    headers: req.headers,
+    method: req.method,
+    json: true,
+    body: req.body,
+  };
 
-  req.pipe(request(url)).pipe(res);
+  req.pipe(request(options)).pipe(res);
 });
 
 const port = 3000; // You can change this port if needed
