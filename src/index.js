6b1b929b95aaac73a104ab59f614c99863c5226a
Bahtijar <no4kar@gmail.com>
2024-06-03 22:43:37 +0200

    return:

--- src/index.js
+++ src/index.js
@@ -10,26 +10,14 @@ const serverUrl = process.env.URL || 'http://ec2-13-53-106-198.eu-north-1.comput
 // Enable CORS
 app.use(cors());
 
-// Use express.json() and express.urlencoded() to handle request bodies
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.all('*', (req, res) => {
   const url = serverUrl + req.url;
-  console.info(`Forwarding ${req.method} request to: ${url}`);
 
-  const options = {
-    url: url,
-    headers: req.headers,
-    method: req.method,
-    json: true,
-    body: req.body,
-  };
+  console.info(url);
 
-  req.pipe(request(options)).pipe(res);
+  req.pipe(request(url)).pipe(res);
 });
 
-const port = 3000; // You can change this port if needed
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
